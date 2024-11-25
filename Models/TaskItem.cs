68223b35b4bdf39beeab932a3fe71324e275d7e1
justnoted <just.noted3505@gmail.com>
2024-11-25 13:49:07 -0600

    Added data annotations

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,10 +1,20 @@
-﻿namespace TaskTracker.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Models;
 
 public class TaskItem
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
     public string Name { get; set; }
+    
     public string? Description { get; set; }
+    
+    [EnumDataType(typeof(Priority))]
     public Priority Priority { get; set; }
+    
+    [DataType(DataType.Date)]
     public DateOnly TargetDate { get; set; }
 }
