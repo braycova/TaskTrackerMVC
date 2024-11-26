4c5f934ccdab0137b7467a67edc82eab6d7b7494
justnoted <just.noted3505@gmail.com>
2024-11-25 21:40:44 -0600

    Updated model field display names

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -8,6 +8,7 @@ public class TaskItem
     public int Id { get; set; }
     
     [Required]
+    [Display(Name = "Task Name")]
     public string Name { get; set; }
     
     public string? Description { get; set; }
@@ -15,6 +16,7 @@ public class TaskItem
     [EnumDataType(typeof(Priority))]
     public Priority Priority { get; set; }
     
+    [Display(Name = "Target Date")]
     [DataType(DataType.Date)]
     public DateOnly TargetDate { get; set; }
 }
